ccf4fd9baa538706c47610027246d3e2c3c76078
amingielewicz <adammingielewicz@gmail.com>
2023-04-05 23:20:12 +0200

    Check out date with datapicker Add assertions to hotel search test

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
     @Test
-    public void getInfo(){
+    public void getInfo() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1230, 730));
@@ -21,9 +24,32 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-        driver.findElement(By.name("checkin")).sendKeys("22/05/2023");
-        driver.findElement(By.name("checkout")).sendKeys("01/06/2023");
+        driver.findElement(By.name("checkin")).sendKeys("22/04/2023");
+        //driver.findElement(By.name("checkout")).sendKeys("01/06/2023");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        List<String> hotelNames  = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b")).stream()
+                .map(e1 -> e1.getAttribute("textContent"))
+                .collect(Collectors.toList());
+        //List<WebElement> hotelNames = driver.findElements(By.cssSelector(("h4[class*='list_title'] > a > b")));
+
+        hotelNames.forEach(System.out::println);
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
+        }
 
 
     }
-}
+
+
